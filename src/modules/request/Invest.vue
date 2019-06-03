c3ef67a9eeb4f79c03ae8d8eadf22be58cb67f16
kennCK <kennettecanales@gmail.com>
2019-06-03 14:11:18 +0800

    Fixed Modal Designs

--- src/modules/request/Invest.vue
+++ src/modules/request/Invest.vue
@@ -2,10 +2,10 @@
   <div class="modal fade" id="createInvestmentModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-sm" role="document">
       <div class="modal-content">
-        <div class="modal-header bg-primary">
+        <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">Invest</h5>
           <button type="button" class="close" @click="hideModal()" aria-label="Close">
-            <span aria-hidden="true" class="text-white">&times;</span>
+            <span aria-hidden="true" class="text-primary">&times;</span>
           </button>
         </div>
         <div class="modal-body">
