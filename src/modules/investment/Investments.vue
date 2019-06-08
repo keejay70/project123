c9a4e62116212ed3835ef8afd4f62dc385fcffbb
kennCK <kennettecanales@gmail.com>
2019-06-08 11:39:59 +0800

    update

--- src/modules/investment/Investments.vue
+++ src/modules/investment/Investments.vue
@@ -1,17 +1,57 @@
 <template>
-  <div class="request-list-wrapper">
-    <div class="request-list-left-container">
+  <div class="investments-list-wrapper">
+    <div class="investments-list-left-container">
 <!--       <div class="rl-container-header">
         <request-filter :size="size"></request-filter>
       </div> -->
-      <div class="rl-container-item" v-for="item, index in data" v-if="data !== null">
-        <span class="summary-header">
+      <div class="il-container-item" v-for="item, index in data" v-if="data !== null">
+<!--         <span class="summary-header">
           {{item.created_at_human}}
-        </span>
+        </span> -->
         <span class="summary-header">
-          <label class="text-primary">
-            <b>PHP {{item.amount.toFixed(2)}}</b>
-          </label>
+          <table class="table table-responsive borderless investment-table">
+            <tr>
+              <td>Requested by</td>
+              <td>
+                {{item.request.account.information.first_name + ' ' + item.request.account.information.last_name}}
+              </td>
+              <td>Reference #</td>
+              <td>
+                <label class="text-primary">{{item.code}}</label>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Principal Amount</td>
+              <td>
+                PHP {{item.request.amount.toFixed(2)}}
+              </td>
+              <td>Approved Date</td>
+              <td>
+                {{item.created_at_human}}
+              </td>
+            </tr>
+            
+            <tr>
+              <td>Invested Amount</td>
+              <td>PHP {{item.amount.toFixed(2)}}</td>
+              <td>Interest Rate</td>
+              <td>
+                {{item.request.interest}}% per month
+              </td>
+            </tr>
+
+            <tr>
+              <td>Returns</td>
+              <td></td>
+              <td>Payable in</td>
+              <td>
+                {{item.request.months_payable}}
+                <label v-if="parseInt(item.request.months_payable) > 1">Months</label>
+                <label v-else>Month</label>
+              </td>
+            </tr>
+          </table>
         </span>
         <span class="body">
           <label v-if="item.message !== null">
@@ -44,13 +84,13 @@
       </div>
       <empty v-if="data === null" :title="'You don\'t have investments right now'" :action="'Go to requested and start investing to our users'" :icon="'far fa-smile'" :iconColor="'text-primary'"></empty>
     </div>
-    <div class="request-list-right-container">
+    <div class="investments-list-right-container">
     </div>
     <profile :item="selecteditem"></profile>
   </div>
 </template>
 <style scoped>
-.request-list-wrapper{
+.investments-list-wrapper{
   width: 100%;
   float: left;
   min-height: 400px;
@@ -59,14 +99,14 @@
   margin-bottom: 100px;
 }
 
-.rl-container-header{
+.il-container-header{
   width: 100%;
   float: left;
   height: 70px;
   border: solid 1px #ddd;
 }
 
-.rl-container-item{
+.il-container-item{
   width: 100%;
   float: left;
   border-radius: 5px;
@@ -77,49 +117,49 @@
   padding-left: 10px;
   padding-right: 10px;
 }
-.rl-container-item .header{
+.il-container-item .header{
   width: 100%;
   float: left;
   height: 50px;
   line-height: 50px;
   color: #555;
 }
-.rl-container-item .summary-header{
+.il-container-item .summary-header{
   width: 100%;
   float: left;
   line-height: 20px;
   color: #555; 
 }
-.rl-container-item .header label, .rl-container-item .summary-header label{
+.il-container-item .header label, .il-container-item .summary-header label{
   margin-bottom: 0px;
 }
-.rl-container-item .body{
+.il-container-item .body{
   width: 100%;
   float: left;
   min-height: 10px;
   overflow-y: hidden;
 }
 
-.rl-container-item .body label{
+.il-container-item .body label{
   margin-bottom: 0px;
   width: 100%;
 }
 
-.rl-container-item .footer{
+.il-container-item .footer{
   width: 100%;
   float: left;
   height: 40px;
   line-height: 40px;
 }
 
-.request-list-left-container{
+.investments-list-left-container{
   float: left;
   width: 68%;
   min-height: 50px;
   overflow-y: hidden;
 }
 
-.request-list-right-container{
+.investments-list-right-container{
   float: left;
   width: 30%;
   min-height: 50px;
@@ -127,11 +167,16 @@
   margin-left: 2%;
 }
 
+.investment-table td{
+  padding-top: 5px !important;
+  padding-bottom: 5px !important;
+}
+
 @media (max-width: 992px){
-  .request-list-wrapper{
+  .investments-list-wrapper{
     margin-bottom: 200px;
   }
-  .request-list-right-container, .request-list-left-container{
+  .investments-list-right-container, .investments-list-left-container{
     width: 100%;
     margin-left: 0%;
   }
