0ef10993bebb0850487819b71394ce77c4b16145
krwntnggn@gmail <krwntnggn@gmail.com>
2019-11-11 16:19:52 +0800

    update

--- src/modules/docs/PrivacyPolicy.vue
+++ src/modules/docs/PrivacyPolicy.vue
@@ -42,7 +42,7 @@
                   </li>
                   <li><strong>Cookies</strong></li>
                   <li>
-                    iLinya may place, view, and/or use cookies, web beacons, local storage objects or similar technologies to collect information about your use of the Program and other websites. This information may include information about the web pages you view, the links you click and other actions you take on our Program and in response to our advertising or email newsletters. iLinya uses the information collected for various reasons, including to remember you and your preferences, track your use of our Program, facilitate your payment transactions, manage access to and use of the Program, and provide a personalized experience.
+                    {{common.COMPANY}} may place, view, and/or use cookies, web beacons, local storage objects or similar technologies to collect information about your use of the Program and other websites. This information may include information about the web pages you view, the links you click and other actions you take on our Program and in response to our advertising or email newsletters. {{common.COMPANY}} uses the information collected for various reasons, including to remember you and your preferences, track your use of our Program, facilitate your payment transactions, manage access to and use of the Program, and provide a personalized experience.
                   </li>
                   <li>
                     A “cookie” is a small text file stored on your computer or other device when you visit certain web pages that record your preferences and actions. Most web browsers automatically accept cookies, but, if you prefer, you can usually modify your browser setting to decline cookies. However, please note that refusing a cookie may limit your access to or use of the Sites.
@@ -51,22 +51,22 @@
               </li>
               <li><strong>SHARING YOUR PERSONAL DATA WITH THIRD PARTIES</strong></li>
               <li>
-                iLinya shares your personal data only when it is necessary to offer the Service, legally required, or permitted by you.
+                {{common.COMPANY}} shares your personal data only when it is necessary to offer the Service, legally required, or permitted by you.
               </li>
               <li>
                 We will provide personal data to hosting providers such as Amazon Web Services, and search engine providers such as Google.
               </li>
               <li>
-                These data processors help us bring you the Service. For example, we may share your information in order to detect where or how you encountered a bug when using our mobile application. In connection with these operations, our service providers will have access to personal data for a limited time. When we utilize service providers for processing any personal data, we implement contractual protections limiting the use of that personal data to the provision of services to iLinya.
+                These data processors help us bring you the Service. For example, we may share your information in order to detect where or how you encountered a bug when using our mobile application. In connection with these operations, our service providers will have access to personal data for a limited time. When we utilize service providers for processing any personal data, we implement contractual protections limiting the use of that personal data to the provision of services to {{common.COMPANY}}.
               </li>
               <li>
                 We will be required to access and disclose personal data in response to lawful requests, such as subpoenas or court orders, or in compliance with applicable laws. Additionally, we will access and share account or other personal data when we believe it is necessary to comply with law, to protect our interests or property, to prevent fraud or other illegal activity perpetrated through the Service, or to prevent imminent harm. This will include accessing and sharing personal data with other companies, lawyers, agents or government agencies.
               </li>
               <li>
-                If the ownership of all or substantially all of the iLinya business, or individual business units or assets owned by iLinya that are related to the Service, were to change, your personal data will be transferred to the new owner. In any such transfer of information, your personal data would remain subject to this section.
+                If the ownership of all or substantially all of the {{common.COMPANY}} business, or individual business units or assets owned by {{common.COMPANY}} that are related to the Service, were to change, your personal data will be transferred to the new owner. In any such transfer of information, your personal data would remain subject to this section.
               </li>
               <li>
-                iLinya will share aggregate or anonymous data collected through the Service, including Activity Data, for purposes such as understanding or improving the service.
+                {{common.COMPANY}} will share aggregate or anonymous data collected through the Service, including Activity Data, for purposes such as understanding or improving the service.
                   <ul>
                     <li><strong>DATA SUBJECT RIGHTS AND DATA RETENTION</strong></li>
                     <li>
@@ -88,30 +88,30 @@
                           Where you have given us consent to process your personal data, you have the right to withdraw that consent at any time.
                         </li>
                         <li>
-                          To export the personal data you have provided to iLinya in a format that can be transferred electronically to a third party.
+                          To export the personal data you have provided to {{common.COMPANY}} in a format that can be transferred electronically to a third party.
                         </li>
                         <li>
-                          To delete your account with iLinya by following the instructions available through the Service.
+                          To delete your account with {{common.COMPANY}} by following the instructions available through the Service.
                         </li>
                       </ul>
                     </li>
                     <li>
-                    iLinya will retain your data until your account is deleted, after which point we will retain anonymous data collected through the Service, including Activity Data, which may be used by iLinya and shared with third parties in any manner.
+                    {{common.COMPANY}} will retain your data until your account is deleted, after which point we will retain anonymous data collected through the Service, including Activity Data, which may be used by {{common.COMPANY}} and shared with third parties in any manner.
                     </li>
                     <li>
                       Please note that some of these rights are not absolute. In some cases, we may refuse a request to exercise particular rights if complying with it meant that we are no longer able to meet our contractual obligation to provide you with particular products and services. We will keep you informed as to the actions that we can take when you make your request.
                     </li>
                     <li><strong>THIRD PARTY WEBSITES AND LINKS</strong></li>
                     <li>
-                      Please note that you may have cookies placed on your computer by third party websites that refer you to our Service. Although we do not share your personal data with these third party websites unless it is reasonably necessary to offer the Service, they may be able to link certain non-personally identifiable information we transfer to them with personal data they previously collected from you. Please review the privacy policies of each website you visit to better understand their privacy practices. In addition, iLinya would like to inform you that anytime you click on links (including advertising banners), which take you to third party websites, you will be subject to the third parties’ privacy policies. Any information submitted by you directly to these third parties is subject to that third party’s privacy policy.
+                      Please note that you may have cookies placed on your computer by third party websites that refer you to our Service. Although we do not share your personal data with these third party websites unless it is reasonably necessary to offer the Service, they may be able to link certain non-personally identifiable information we transfer to them with personal data they previously collected from you. Please review the privacy policies of each website you visit to better understand their privacy practices. In addition, {{common.COMPANY}} would like to inform you that anytime you click on links (including advertising banners), which take you to third party websites, you will be subject to the third parties’ privacy policies. Any information submitted by you directly to these third parties is subject to that third party’s privacy policy.
                     </li>
                     <li><strong>LINKS</strong></li>
                     <li>
-                      The Service may contain links to other websites. We are not responsible for the privacy practices of other websites. We encourage users to be aware when they leave the Service to read the privacy statements of other websites that collect personally identifiable information. This Privacy Policy applies only to information collected by iLinya via the Service.
+                      The Service may contain links to other websites. We are not responsible for the privacy practices of other websites. We encourage users to be aware when they leave the Service to read the privacy statements of other websites that collect personally identifiable information. This Privacy Policy applies only to information collected by {{common.COMPANY}} via the Service.
                     </li>
                     <li><strong>INFORMATION SECURITY</strong></li>
                     <li>
-                      iLinya has implemented administrative and technical safeguards it believes are appropriate to protect the confidentiality, integrity and availability of your personal data, User Photo, and access credentials. However, given sufficient resources, a determined attacker could defeat those safeguards and may, as a result, gain access to the data we seek to protect.
+                      {{common.COMPANY}} has implemented administrative and technical safeguards it believes are appropriate to protect the confidentiality, integrity and availability of your personal data, User Photo, and access credentials. However, given sufficient resources, a determined attacker could defeat those safeguards and may, as a result, gain access to the data we seek to protect.
                     </li>
                     <li><strong>DO NOT TRACK</strong></li>
                     <li>
@@ -119,7 +119,7 @@
                     </li>
                     <li><strong>CONTACT US</strong></li>
                     <li>
-                      For all data privacy and support inquiries and any questions or concerns you have about this Privacy Policy, please contact iLinya here (support@classworx.co)
+                      For all data privacy and support inquiries and any questions or concerns you have about this Privacy Policy, please contact {{common.COMPANY}} here {{(common.APP_EMAIL)}}
                     </li>
                   </ul>
               </li>
@@ -164,10 +164,12 @@ ul li{
   import ROUTER from 'src/router'
   import Vue from 'vue'
   import AUTH from 'src/services/auth'
+  import COMMON from 'src/common.js'
   export default{
     data(){
       return {
-        user: AUTH.user
+        user: AUTH.user,
+        common: COMMON
       }
     },
     props: {
