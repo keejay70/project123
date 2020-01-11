bbe3e830e39f7748edc9d8e089f48fcc1909065e
kennCK <kennettecanales@gmail.com>
2020-01-11 14:07:09 +0800

    update

--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@ import App from './App'
 import router from './router'
 import config from './config.js'
 /* eslint-disable no-new */
-Vue.use(require('vue-pusher'), {
-  api_key: config.PUSHER_KEY,
-  options: {
-    cluster: 'ap1',
-    encrypted: true
-  }
-})
+if(config.PUSHER.flag === 'pusher'){
+  Vue.use(require('vue-pusher'), {
+    api_key: config.PUSHER.key,
+    options: {
+      cluster: 'ap1',
+      encrypted: true
+    }
+  })
+}
 new Vue({
   el: '#app',
   router,
